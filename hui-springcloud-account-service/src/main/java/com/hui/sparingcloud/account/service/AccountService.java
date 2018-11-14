dbe9243ba0d861b744949ce7dbb7791c69bfcd33
ithuhui <hu545548724@163.com>
2018-11-14 22:09:42 +0800

    chore : init the project spring cloud V1.1 继续完善spring cloud 微服务骨架搭建。

---
+++ hui-springcloud-account-service/src/main/java/com/hui/sparingcloud/account/service/AccountService.java
@@ -0,0 +1,13 @@
+package com.hui.sparingcloud.account.service;
+
+/**
+ * <b><code>AccountService</code></b>
+ * <p/>
+ * Description:
+ * <p/>
+ * <b>Creation Time:</b> 2018/11/13 21:50.
+ *
+ * @author Hu Weihui
+ */
+public class AccountService {
+}
---
+++ hui-springcloud-account-service/src/main/java/com/hui/sparingcloud/account/service/impl/AccountServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hui.sparingcloud.account.service.impl;
+
+/**
+ * <b><code>AccountServiceImpl</code></b>
+ * <p/>
+ * Description:
+ * <p/>
+ * <b>Creation Time:</b> 2018/11/13 21:50.
+ *
+ * @author Hu Weihui
+ */
+public class AccountServiceImpl {
+}

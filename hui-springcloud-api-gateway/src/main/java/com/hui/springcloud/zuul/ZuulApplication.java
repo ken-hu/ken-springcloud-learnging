dfb857253473e849a78a0fc0a861723d8dcff03e
ithuhui <hu545548724@163.com>
2018-12-01 13:46:02 +0800

    chore: add zuul demo

--- hui-springcloud-api-gateway/src/main/java/com/hui/springcloud/zuul/ZuulApplication.java
+++ hui-springcloud-api-gateway/src/main/java/com/hui/springcloud/zuul/ZuulApplication.java
@@ -1,5 +1,9 @@
 package com.hui.springcloud.zuul;
 
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
 /**
  * <b><code>ZuulApplication</code></b>
  * <p/>
@@ -9,5 +13,8 @@ package com.hui.springcloud.zuul;
  *
  * @author Hu Weihui
  */
+@SpringCloudApplication
+@EnableEurekaClient
+@EnableZuulServer
 public class ZuulApplication {
 }
